99a6143f96fd06fce53cbf8f48824b60cbf69265
sp-devi <spneuro01@gmail.com>
2020-08-18 00:11:19 +0900

    fix basic layout of calendar day info

--- Hoopsy/src/components/EventCalendarDate.js
+++ Hoopsy/src/components/EventCalendarDate.js
@@ -3,20 +3,38 @@ import { View, StyleSheet, Text } from 'react-native';
 
 export default class EventCalendarDate extends Component {
 
+    getYear() {
+        return "2020";
+    }
+
     getMonth() {
-        return (
-            <View>
-                {this.props.children}
-            </View>
-        );
+        return "NOV";
+    }
+
+    getDay() {
+        return "3";
+    }
+
+    getDayName() {
+        return "Tue"
     }
 
     render() {
         return (
             <View style={styles.calendarDateContainer}>
                 <View style={styles.calendarDateMonth}>
-                    <Text>
+                    <Text style={styles.calendarDateMonthText}>
                         {this.getMonth()}
+                    </Text>
+                </View>
+                <View style={styles.calendarDateDay}>
+                    <Text style={styles.calendarDateDayText}>
+                        {this.getDay()}
+                    </Text>
+                </View>
+                <View style={styles.calendarDateDayName}>
+                    <Text style={styles.calendarDateDayNameText}>
+                        {this.getDayName()}
                     </Text>
                 </View>
             </View>
@@ -26,6 +44,31 @@ export default class EventCalendarDate extends Component {
 // </EventCard>
 const styles = StyleSheet.create({
     calendarDateContainer: {
-        flexDirection: "column"
+        flexDirection: "column",
+        width: 50,
+        borderWidth: 1,
+        
+    },
+    calendarDateMonth: {
+        borderColor: "black",
+    },
+    calendarDateMonthText: {
+        textAlign: "center"
+    },
+    calendarDateDay: {
+        borderColor: "black",
+        borderTopWidth: 1,
+        height: 50,
+        justifyContent: "center"
+    },
+    calendarDateDayText: {
+        textAlign: "center"
+    },
+    calendarDateDayName: {
+        borderColor: "black",
+        borderTopWidth: 1
+    },
+    calendarDateDayNameText: {
+        textAlign: "center"
     }
 });
