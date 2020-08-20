879006bd6227bbc3d43ec3ba552c94ddf10e90a4
sp-devi <spneuro01@gmail.com>
2020-08-20 23:18:47 +0900

    revised SportEvent component, refactor out EventCard

--- Hoopsy/src/components/SportEvent.js
+++ Hoopsy/src/components/SportEvent.js
@@ -2,23 +2,39 @@ import React, { Component } from 'react';
 import { View, StyleSheet, Text, ImageBackground, Button } from 'react-native';
 
 import EventCard from './EventCard';
-import EventCalendarDate  from './EventCalendarDate';
+import EventCalendarDate from './EventCalendarDate';
 
 export default class SportEvent extends Component {
 
     render() {
         return (
-            <EventCard>
-                <View style={styles.eventDate}>
-                    <EventCalendarDate>
-                        November 30, 2020
-                    </EventCalendarDate>
-                </View>
-            </EventCard>
+            <View>
+                <EventCard
+                    cardTitle="Sp Event"
+                    eventDate="Nov 12, 2009"
+                    eventTime={{
+                        start: "19:00",
+                        end: "20:00"
+                    }}>
+                </EventCard>
+            </View>
         );
     }
 }
 // </EventCard>
 const styles = StyleSheet.create({
-
+    eventDateContainer: {
+        flex: 1,
+        flexDirection: "row"
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center"
+    },
+    title: {
+        flex: 1
+    }
 });
